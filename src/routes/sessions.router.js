8378d530e24e27c666800c6a35988d08b2285b19
TomasGon1 <xnicksyux@gmail.com>
2024-03-18 12:29:39 -0300

    login-github

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -39,4 +39,21 @@ router.get("/logout", (req, res) => {
   res.redirect("/login");
 });
 
+//Login GitHub
+router.get(
+  "/github",
+  passport.authenticate("github", { scope: ["user:email"] }),
+  async (req, res) => {}
+);
+
+router.get(
+  "/githubcallback",
+  passport.authenticate("github", { failureRedirect: "login" }),
+  async (req, res) => {
+    req.session.user = req.user;
+    req.session.login = true;
+    req.redirect("/profile");
+  }
+);
+
 module.exports = router;
