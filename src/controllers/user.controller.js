a588111e7d251d057d4f85f428822ac69b3297ae
TomasGon1 <xnicksyux@gmail.com>
2024-05-16 21:32:02 -0300

    nuevouser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,10 +4,23 @@ const CartModel = require("../models/cart.model.js");
 const passport = require("passport");
 const {createHash, isValidPassword} = require("../utils/hashbcrypt.js");
 
+//Errores custom
+const {
+  registerInfoError,
+  loginInfoError,
+} = require("../services/errors/info.js");
+const { EErrors } = require("../services/errors/enums.js");
+const CustomError = require("../services/errors/custom-error.js");
+
 class UserController {
   static async register(req, res) {
     try {
-      
+      const {first_name, last_name, email, password, age} = req.body;
+
+      const existingUser = await UserModel.finndOne({email});
+      if(existingUser) {
+        
+      }
     } catch (error) {
       
     }
