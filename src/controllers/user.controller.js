79fa70221deed0ce73e97c43621e305399935a2f
TomasGon1 <xnicksyux@gmail.com>
2024-05-16 22:01:32 -0300

    nuevouser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -47,15 +47,76 @@ class UserController {
   }
 
   async login(req, res) {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
     try {
-      const userFound = await UserModel.findOne({email});
-      if(!userFound) {
-        
+      const userFound = await UserModel.findOne({ email });
+      if (!userFound) {
+        throw CustomError.createError({
+          name: "Login fail",
+          cause: loginInfoError({ email, password }),
+          message: "Error al intentar logearse, usuario invalido!",
+          code: EErrors.USER_IVALID,
+        });
       }
+
+      const isValid = isValidPassword(password, userFound);
+      if (!isValid) {
+        throw CustomError.createError({
+          name: "Login fail",
+          cause: loginInfoError({ email, password }),
+          message: "Error al intentar logearse, contraseña invalida!",
+          code: EErrors.USER_IVALID,
+        });
+      }
+
+      res.redirect("/api/users/profile");
     } catch (error) {
-      
+      console.error(error);
+      res.status(500).send("Error interno del servidor");
     }
+  }
+
+  async profile(req, res) {
+    const userDTO = new UserDTO(
+      req.user.first_name,
+      req.user.last_name,
+      req.user.role
+    );
+    const isAdmin = req.user.role === "admin";
+    res.render("profile", { user: userDTO, isAdmin });
+  }
+
+  async logout(req, res) {
+    req.logout();
+    res.redirect("/login");
+  }
+
+  async loginGitHub(req, res) {
+    passport.authenticate("github", { scope: ["user:email"] })(req, res, next);
+  }
+
+  async loginGitHubCallback(req, res) {
+    passport.authenticate("github", (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.redirect("/login");
+      }
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        return res.redirect("/api/users/profile");
+      });
+    })(req, res, next);
+  }
+
+  async admin(req, res) {
+    if (req.user.user.role !== "admin") {
+      return res.status(403).send("Acceso denegado");
+    }
+    res.render("admin");
   }
 }
 
--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -6,18 +6,21 @@ const UserController = require("../controllers/user.controller.js");
 const userController = new UserController();
 
 //Registro
-
+router.post("/register", userController.register);
 
 //Login
-
+router.post("/login", userController.login);
 
 //Perfil
-
+router.get("/profile", passport.authenticate("local", {session: false}), userController.profile);
 
 //Logout
-
+router.post("/logout", userController.logout.bind(userController));
 
 //Github
 
+
+//Admin
+router.get("/admin", userController.admin);
 
 module.exports = router;
